0fc0b9fded35b7128088b06dafb96c4315059258
huangshuai <huangshuai@ctfo.com>
2024-02-02 14:52:21 +0800

    dolphinscheduler: 升级到 3.2.0

--- ansible/roles/dolphinscheduler/files/collect-files.sh
+++ ansible/roles/dolphinscheduler/files/collect-files.sh
@@ -8,11 +8,11 @@ echo "Collect dolphinscheduler images..."
 echo "    pull dolphinscheduler images..."
 docker pull bitnami/postgresql:11.11.0 > /dev/null
 docker pull bitnami/zookeeper:3.6.2 > /dev/null
-docker pull apache/dolphinscheduler-tools:3.1.8 > /dev/null
-docker pull apache/dolphinscheduler-api:3.1.8 > /dev/null
-docker pull apache/dolphinscheduler-alert-server:3.1.8 > /dev/null
-docker pull apache/dolphinscheduler-master:3.1.8 > /dev/null
-docker pull apache/dolphinscheduler-worker:3.1.8 > /dev/null
+docker pull apache/dolphinscheduler-tools:3.2.0 > /dev/null
+docker pull apache/dolphinscheduler-api:3.2.0 > /dev/null
+docker pull apache/dolphinscheduler-alert-server:3.2.0 > /dev/null
+docker pull apache/dolphinscheduler-master:3.2.0 > /dev/null
+docker pull apache/dolphinscheduler-worker:3.2.0 > /dev/null
 
 rm -f ${SCRIPT_HOME}/dolphinscheduler-images.tar.gz
 
@@ -20,11 +20,11 @@ echo "    save dolphinscheduler images..."
 docker save \
     bitnami/postgresql:11.11.0 \
     bitnami/zookeeper:3.6.2 \
-    apache/dolphinscheduler-tools:3.1.8 \
-    apache/dolphinscheduler-api:3.1.8 \
-    apache/dolphinscheduler-alert-server:3.1.8 \
-    apache/dolphinscheduler-master:3.1.8 \
-    apache/dolphinscheduler-worker:3.1.8 \
+    apache/dolphinscheduler-tools:3.2.0 \
+    apache/dolphinscheduler-api:3.2.0 \
+    apache/dolphinscheduler-alert-server:3.2.0 \
+    apache/dolphinscheduler-master:3.2.0 \
+    apache/dolphinscheduler-worker:3.2.0 \
 | gzip --stdout > ${SCRIPT_HOME}/dolphinscheduler-images.tar.gz
 
 echo "    download datax..."
